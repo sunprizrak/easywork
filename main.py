370eac94c15adf56d348214cc51f25b530e152f5
sunprizrak <sunprizrak@gmail.com>
2023-08-09 18:49:32 +0300

    ...

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ class CustomThemeManager(ThemeManager):
             "Button": ["GlossySheen", 21, True, 1.25],
         })
 
-        font_path = os.sep.join(['assets', 'font', 'GlossySheenRegular.ttf'])
+        font_path = os.path.abspath(os.sep.join(['assets', 'font', 'GlossySheenRegular.ttf']))
         print(font_path)
 
         LabelBase.register(name='GlossySheen', fn_regular=font_path)
@@ -36,7 +36,7 @@ class MainApp(MDApp):
         self.dialog = None
 
     def build(self):
-        layout_path = os.sep.join(['kv_files', 'layout.kv'])
+        layout_path = os.path.abspath(os.sep.join(['kv_files', 'layout.kv']))
 
         kv_file = Builder.load_file(layout_path)
         return kv_file
