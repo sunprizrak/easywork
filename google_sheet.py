06bd465fd531e1a6443706c6c96c836d3fda6432
sunprizrak <sunprizrak@gmail.com>
2023-08-10 23:26:14 +0300

    ...

--- google_sheet.py
+++ google_sheet.py
@@ -54,7 +54,7 @@ class GoogleSheet:
                         cell_list = worksheet.range(column_range)
 
                         for i, cell in enumerate(cell_list):
-                            cell.value = val[4:][i]
+                            cell.value = int(val[4:][i])
 
                         worksheet.update_cells(cell_list)
 
