ee6519bc5ee0972391bf0b4af48a6f395791b1fd
sunprizrak <sunprizrak@gmail.com>
2023-08-17 19:30:30 +0300

    ...

--- google_sheet.py
+++ google_sheet.py
@@ -35,34 +35,33 @@ class GoogleSheet:
 
         for worksheet in all_worksheets:
             try:
-                data = worksheet.get_all_values()
                 print(f'worksheet: {worksheet}')
 
                 patterns = ['vpipovrll', 'pfrovrll', '3\D*betovrll', 'c\D*betovrll', 'handsovrll', 'dateovrll']
 
-                columns = [max(walrus) for pattern in patterns if (walrus := [i for i, el in enumerate(data[0], 1) if re.fullmatch(pattern, el.lower().replace(' ', ''))])]
+                for key, val in data_table.items():
+                    rows_number = [cell.row for cell in worksheet.findall(key)]
 
-                if len(columns) == len(patterns):
+                    if len(rows_number) > 0:
+                        data = worksheet.get_all_values()
+                        columns = [max(walrus) for pattern in patterns if (walrus := [i for i, el in enumerate(data[0], 1) if re.fullmatch(pattern, el.lower().replace(' ', ''))])]
 
-                    for key, val in data_table.items():
-                        rows_number = [cell.row for cell in worksheet.findall(key)]
+                        if len(columns) == len(patterns):
+                            for row in rows_number:
+                                hands_value = worksheet.cell(row, columns[-2]).value
 
-                        for row in rows_number:
-                            hands_value = worksheet.cell(row, columns[-2]).value
+                                if type(hands_value) is str:
+                                    if hands_value.replace(',', '').isdigit() and int(val[-2]) < int(hands_value.replace(',', '')):
+                                        continue
 
-                            if type(hands_value) is str:
-                                if hands_value.replace(',', '').isdigit() and int(val[-2]) < int(hands_value.replace(',', '')):
-                                    continue
+                                column_range = f'{rowcol_to_a1(row, columns[0])}:{rowcol_to_a1(row, columns[-1])}'
+                                cell_list = worksheet.range(column_range)
 
-                            column_range = f'{rowcol_to_a1(row, columns[0])}:{rowcol_to_a1(row, columns[-1])}'
-                            cell_list = worksheet.range(column_range)
+                                for i, cell in enumerate(cell_list):
+                                    cell.value = int(val[4:][i]) if '/' not in val[4:][i] else val[4:][i]
 
-                            for i, cell in enumerate(cell_list):
-                                cell.value = int(val[4:][i]) if '/' not in val[4:][i] else val[4:][i]
-
-                            worksheet.update_cells(cell_list)
-                            print(f'update: {worksheet}')
-                time.sleep(10)
+                                worksheet.update_cells(cell_list)
+                                print(f'update: {worksheet}')
             except Exception as error:
                 print(error)
                 print(worksheet)
