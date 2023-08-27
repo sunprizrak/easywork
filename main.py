1007868fdcc4449df511be0ab720a9f9cb5e2afb
sunprizrak <sunprizrak@gmail.com>
2023-08-27 23:48:53 +0300

    ...

--- main.py
+++ main.py
@@ -20,10 +20,7 @@ if __name__ == '__main__':
                 "Button": ["GlossySheen", 21, True, 1.25],
             })
 
-            path = os.path.abspath('.')
-
-            print(f'abspath: {path}')
-            print(os.listdir(path="."))
+            print(os.sep.join(['assets', 'font', 'GlossySheenRegular.ttf']))
 
             if os.path.exists(os.sep.join(['assets', 'font', 'GlossySheenRegular.ttf'])):
                 print('Такой файл существует !!!!!!!!!!!!!!!!!!!!!!!!!!!')
